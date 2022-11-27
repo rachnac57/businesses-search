c18c85d5216b28534d239818ac14ce8604f12ae2
Rachna <38106843+rachnac57@users.noreply.github.com>
2022-11-28 02:22:34 +0530

    Blank rating story handling

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -1,9 +1,14 @@
 import { StarHalf } from '@mui/icons-material';
 import StarIcon from '@mui/icons-material/Star';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 
 export default function Ratings(props) {
     let ratingsList = [];
     const {ratings} = props;
+    // For creating a story for blank rating
+    if (!ratings) {
+        return <ThumbDownOffAltIcon/>;
+    }
     if (Number.isInteger(ratings)) {
         ratingsList = new Array(ratings).fill(1);
     } else {
