c28249d8978bf11478ae92243687be8db5d7751c
Rachna <38106843+rachnac57@users.noreply.github.com>
2022-11-27 23:38:21 +0530

    Ratings component

---
+++ src/components/Ratings.js
@@ -0,0 +1,19 @@
+import { StarHalf } from '@mui/icons-material';
+import StarIcon from '@mui/icons-material/Star';
+
+export default function Ratings(props) {
+    let ratingsList = [];
+    const {ratings = []} = props;
+    if (Number.isInteger(ratings)) {
+        ratingsList = new Array(ratings).fill(1);
+    } else {
+        ratingsList = new Array(Math.floor(ratings)).fill(1);
+        ratingsList.push(0.5);
+    }
+    return ratingsList.map((rating, index) => {
+        if (rating === 1) {
+            return <StarIcon key={`rating-${index}`} />;
+        }
+        return <StarHalf key={`rating-${index}`} />;
+    });
+}
